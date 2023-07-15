ef8b064e4533d3812282356a8a74a2140f0954b4
emirulmukmin <emirulmukmin@gmail.com>
2023-07-16 00:01:56 +0700

    fixing show toast message bug

--- app/src/main/java/com/dicoding/gituserlite/ui/view/DetailActivity.kt
+++ app/src/main/java/com/dicoding/gituserlite/ui/view/DetailActivity.kt
@@ -155,7 +155,7 @@ class DetailActivity : AppCompatActivity(), View.OnClickListener {
                 } else {
                     detailUsername?.let { detailViewModel.insertFavorite(it) }
                     isFavoriteUser(true)
-                    showToast(this, "User Deleted from Favorite")
+                    showToast(this, "User Added to Favorite")
                 }
             }
         }
